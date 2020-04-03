bca3aa72a2f4587955a1ce7dc97ab26cba5934d3
Radim Vansa <rvansa@redhat.com>
2020-04-03 18:34:28 +0200

    Add prefix & suffix, delete tmp input

--- src/main/resources/lighttpd/convert.sh
+++ src/main/resources/lighttpd/convert.sh
@@ -5,9 +5,19 @@ INPUT=$(mktemp /tmp/input.XXXXXX)
 cat > $INPUT
 ID=$(jq -r .info.id $INPUT)
 BENCHMARK=$(jq -r .info.benchmark $INPUT)
-OUTPUT=/var/www/localhost/htdocs/$ID-$BENCHMARK.html
+
+declare -A QUERY_PARAMS
+while IFS='=' read -r -d '&' key value && [[ -n "$key" ]]; do
+    QUERY_PARAMS["$key"]=$value
+done <<<"${QUERY_STRING}&"
+PREFIX=${QUERY_PARAMS["prefix"]:-""}
+SUFFIX=${QUERY_PARAMS["suffix"]:-""}
+
+FILE=${PREFIX}${ID}-${BENCHMARK}${SUFFIX}.html
+OUTPUT=/var/www/localhost/htdocs/$FILE
 awk '/\*\*DATAKEY\*\*/ { printf "       window.__DATA__ = "; system("cat '$INPUT'"); next; } /.*/ { print; }' < $TEMPLATE > $OUTPUT
+rm $INPUT
 
 echo "Status: 307 Temporary redirect"
-echo "Location: /$ID-$BENCHMARK.html"
+echo "Location: /$FILE"
 echo ""
