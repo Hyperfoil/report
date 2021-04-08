afe249cfb686e96a50dcd4f5e871ed3d92b6019a
Radim Vansa <rvansa@redhat.com>
2021-04-08 11:34:23 +0200

    Allow unwrap components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,6 +12,7 @@ import Header from './Header';
 
 import { history } from '../redux/store';
 import { getAlerts, getData } from '../redux/selectors';
+import * as qs from 'query-string';
 
 const logoProps = {
     href: '/',
@@ -22,28 +23,39 @@ const logoProps = {
     //target: '_blank'
 };
 
-export default ({ children }) => {
+function Content({ children }) {
     const alerts = useSelector(getAlerts);
     const data = useSelector(getData)
+    return <>
+        {alerts.map(({ title, variant = "danger", message }, index) => (
+            <Alert
+                key={index}
+                variant={variant}
+                title={title}
+            // action={<AlertActionCloseButton onClose={() => { }} />}
+            >
+                <div dangerouslySetInnerHTML={{ __html: message }} />
+
+            </Alert>
+        ))}
+        {data ? children : alerts.length === 0 ?
+            (<Bullseye>
+                <Spinner />
+            </Bullseye>) : null
+        }
+    </>
+}
+
+export default ({ children }) => {
+    // useHistory, useLocation and useParams don't work for some reason
+    const params = qs.parse(window.location.search)
+    if (params.unwrap === "true") {
+        return <Content children={children} />
+    }
     return (
         <Page header={(<Header logoProps={logoProps} />)}>
             <PageSection isFilled={true}>
-                {alerts.map(({ title, variant = "danger", message }, index) => (
-                    <Alert
-                        key={index}
-                        variant={variant}
-                        title={title}
-                    // action={<AlertActionCloseButton onClose={() => { }} />}
-                    >
-                        <div dangerouslySetInnerHTML={{ __html: message }} />
-
-                    </Alert>
-                ))}
-                {data ? children : alerts.length === 0 ?
-                    (<Bullseye>
-                        <Spinner />
-                    </Bullseye>) : null
-                }
+                <Content children={children} />
             </PageSection>
         </Page>
     )
